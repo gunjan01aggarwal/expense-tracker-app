760706c913f46dcc6fc5d6b16eac097d6223528c
Gunjan <gunjan01aggarwal@gmail.com>
2025-11-11 11:14:50 +0530

    add requirements.txt

--- expense_pro/expense_pro/settings.py
+++ expense_pro/expense_pro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&l6onmb=3*y)(*1-%1zozt$r$+o=%=+-c=h@!)als*u%w)wwxz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost']
 
 
 # Application definition
